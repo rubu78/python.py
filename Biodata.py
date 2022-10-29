35350d5f72c9ec59ab26b85f30d59506d2f6c31d
rubu78 <116946461+rubu78@users.noreply.github.com>
2022-10-29 14:26:38 +0530

    Add files via upload

---
+++ Biodata.py
@@ -0,0 +1,32 @@
+#PROGRAM TO BUILD A BIODATA
+'''Name = input("enter the Name :")
+Cell = input("enter the cell number :")
+Mail = input("enter the Mail Id :")
+Academia = input("enter qualification :")
+Father = input("enter father's name :")
+DOB = input("enter date of birth :")
+Meritalstatus = input("enter Merital Stats :")
+language = input("enter Language Known :")
+Hobbies = input("enter Hobbies :")
+Address = input("enter Permanent Address :")
+
+
+print("---------------BIO DATA---------------")
+
+print("Name".ljust(20),":",Name.strip())
+print("Cell No.".ljust(20),":",Cell.strip())
+print("Mail Id.".ljust(20),":",Mail.strip())
+print("Academia".ljust(20),":",Academia.strip())
+
+print("----Personal Details----")
+
+print("Father Name".ljust(20),":",Father.strip())
+print("D.O.B".ljust(20),":",DOB.strip())
+print("Merital Status".ljust(20),":",Meritalstatus.strip())
+print("Language Known".ljust(20),":",language.strip())
+print("Hobbies".ljust(20),":",Hobbies.strip())
+print("Permanent Address".ljust(20),":",Address.strip())'''
+
+
+x,y,z=["apple","banana","orrange"]
+print(x)
